82309263e2a2dde480e53347763807170682d487
Lalit Sangore <96982204+lalitt08@users.noreply.github.com>
2025-01-31 01:21:59 +0530

    Update clean_old_files.sh

--- scripts/clean_old_files.sh
+++ scripts/clean_old_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e  # Stop script if any command fails
 
-echo "Deleting old application files..."
-rm -rf /Django_Chatapp/*  # Deletes all files inside Django_Chatapp
+echo "Deleting old application files but keeping venv..."
+find /Django_Chatapp -mindepth 1 ! -name 'venv' ! -name 'scripts' -exec rm -rf {} +
 echo "Old files deleted successfully!"
