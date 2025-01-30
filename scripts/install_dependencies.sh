2aff03f1a968b62249083cbf1cc9b52fb147cf1e
Lalit Sangore <96982204+lalitt08@users.noreply.github.com>
2025-01-30 16:55:59 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e  # Stop script if any command fails
+
 echo "Installing dependencies..."
-cd /Django_chatapp
+cd /Django_Chatapp  # Go to the existing project directory
+
+# Activate virtual environment
 source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
+
+echo "Dependencies installed successfully!"
