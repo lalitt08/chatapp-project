b571d045cd54cc6e9cc3ccabb80e6bdcf3cd6f37
Lalit Sangore <96982204+lalitt08@users.noreply.github.com>
2025-01-31 01:31:43 +0530

    Update and rename clean_old_files.sh to clean_directory.sh

--- scripts/clean_directory.sh
+++ scripts/clean_directory.sh
@@ -4,3 +4,5 @@ set -e  # Stop script if any command fails
 echo "Deleting old application files but keeping venv..."
 find /Django_Chatapp -mindepth 1 ! -name 'venv' ! -name 'scripts' -exec rm -rf {} +
 echo "Old files deleted successfully!"
+echo "Cleaning existing Django_Chatapp directory..."
+rm -rf /Django_Chatapp/fundoo/fundoo/urls.py
